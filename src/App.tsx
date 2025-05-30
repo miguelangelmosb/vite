d0007cf863916a2220e987058da995b46fc57743
miguelangelmosb <miguelangelmosb@gmail.com>
2025-05-29 22:57:24 -0500

    Added Card component with personal info, setting a fixed width and dynamic text size

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
+      <Card>
+        <CardHeader>
+          <CardTitle>Miguel Mosquera</CardTitle>
+          <CardDescription>Software Engineer</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>I am a software engineer passionate about learning, sharing knowledge, and collaborating in teams. I enjoy taking on new challenges, exploring emerging technologies, and contributing to collective growth, driven by curiosity and a constant desire to improve.</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
     </div>
   )
 }
-
 export default App
